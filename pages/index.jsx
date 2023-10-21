e6adbfa5530c009e8d5db051d289cb3ee592c9ee
panpayak <daigokokawa@gmail.com>
2023-10-21 17:16:52 +0900

    ✨ useStateの文字列、真偽値について練習

--- pages/index.jsx
+++ pages/index.jsx
@@ -8,12 +8,26 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   const [count, setCount] = useState(1);
+  const [text, setText] = useState("");
+  const [isShow, setIsShow] = useState(true);
 
   const handleClick = useCallback(() => {
     if (count < 10) {
       setCount((count) => count + 1);
     }
   }, [count]);
+
+  const handleChange = useCallback((e) => {
+    if (e.target.value.length > 5) {
+      alert("５文字以内にしてください");
+      return;
+    }
+    setText(e.target.value.trim().toLowerCase());
+  }, []);
+
+  const handleDisplay = useCallback(() => {
+    setIsShow((isShow) => !isShow);
+  }, []);
 
   useEffect(() => {
     // マウント
@@ -31,8 +45,10 @@ export default function Home() {
         <title>Index Page</title>
       </Head>
       <Header />
-      <h1>{count}</h1>
+      {isShow ? <h1>{count}</h1> : null}
+      <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <button onClick={handleClick}>ボタン</button>
+      <input type="text" value={text} onChange={handleChange} />
       <Main page="index" />
     </>
   );
