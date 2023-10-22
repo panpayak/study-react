3b4f9b08a4fa8726c92d4c64c001a8f95d729232
panpayak <daigokokawa@gmail.com>
2023-10-22 15:14:43 +0900

    ✨ useMemoについて練習

--- pages/about.jsx
+++ pages/about.jsx
@@ -8,7 +8,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 
 export default function About({
-  count,
+  doubleCount,
   isShow,
   handleClick,
   handleDisplay,
@@ -24,7 +24,7 @@ export default function About({
         <title>About Page</title>
       </Head>
       <Header />
-      {isShow ? <h1>{count}</h1> : null}
+      {isShow ? <h1>{doubleCount}</h1> : null}
       <button onClick={handleDisplay}>{isShow ? "非表示" : "表示"}</button>
       <button onClick={handleClick}>ボタン</button>
       <input type="text" value={text} onChange={handleChange} />
